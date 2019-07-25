bbf7ab78db132a3d762c7bb9ff60cd8334abe351
abhishek <abhishekak98148@gmail.com>
2019-07-25 23:33:47 +0530

    solved

--- chapter_17/17.18_SmallestSuperSequence.cpp
+++ chapter_17/17.18_SmallestSuperSequence.cpp
@@ -4,6 +4,7 @@
 */
 
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 // elements will be in order
@@ -26,6 +27,49 @@ void print(int bigger[], int n, int smaller[], int m, int element,int start = -1
      print(bigger+1,n-1,smaller,m,element,start,index+1);
 }
 
+int smallestSuperSequence(int bigger[], int smaller[], int n, int m){
+     unordered_map<int,bool> map;
+     for(int i = 0; i < m; i++)
+          map[smaller[i]] = true;
+
+     int start = 0, end = 999999999;
+
+     for(int i = 0; i < n - 2; i++){
+          bool found = true;
+          if(map.count(bigger[i])){
+               int j;
+               int count = 0;
+               for(j = i; j < n; j++){
+                    if(map.count(bigger[j])){
+                         if(map[bigger[j]]){
+                              map[bigger[j]] = false;
+                              count += 1;
+                         }
+                         else{
+                              found = false;
+                              break;
+                         }
+                    }
+
+                    if(count == map.size())
+                         break;
+               }
+               if(found){
+                    if((end - start) > (j - i)){
+                         start = i;
+                         end = j;
+                         std::cout << i << " " << j << '\n';
+                    }
+               }
+
+               for(int j = 0; j < map.size(); j++)
+                    map[smaller[j]] = true;
+          }
+     }
+     std::cout << start << " "  << end << '\n';
+     return 0;
+}
+
 int main(){
      int n;
      cin >> n;
@@ -40,7 +84,8 @@ int main(){
      int bigger[m];
      for(int i = 0; i < m; i++)
           cin >> bigger[i];
-     print(bigger,m,smaller,n,smaller[0]);
+     //print(bigger,m,smaller,n,smaller[0]);
+     std::cout << smallestSuperSequence(bigger,smaller,m,n) << '\n';
 }
 /*
 3
